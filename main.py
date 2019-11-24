a0c70522b652af824d0586636a2685caef3d166a
tstctzyh <nik7880@gmail.com>
2019-11-24 23:15:55 +0700

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import pickle
+
+sns.set()
+
+def main():
+	print(123)
